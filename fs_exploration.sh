136def2e1405e827aca943d80d2b1680a35d4501
David-ui32 <cristo123456xx@gmail.com>
2026-04-02 12:58:14 +0000

    sudo

---
+++ fs_exploration.sh
@@ -0,0 +1,10 @@
+# 1 movernor al directorio raiz
+cd /
+# 2 listo enlaces simbolicos
+ls -F
+sudo apt update # actualiza con exito el repositorio
+
+apt upgrade # actualiza 
+
+apt install parted #instalar el  paquete en partes
+sudo parted -l && echo -e "\n---\n" && lsblk -f && echo -e "\n---\n" #Me da la informacion del sector y del disco y todos los datos 
