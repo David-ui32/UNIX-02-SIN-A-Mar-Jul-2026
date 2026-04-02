7da3667998cf40a0b7ede9a2a4800c585894a0c9
David-ui32 <cristo123456xx@gmail.com>
2026-04-02 13:04:03 +0000

    lsblk

--- fs_exploration.sh
+++ fs_exploration.sh
@@ -8,3 +8,5 @@ apt upgrade # actualiza
 
 apt install parted #instalar el  paquete en partes
 sudo parted -l && echo -e "\n---\n" && lsblk -f && echo -e "\n---\n" #Me da la informacion del sector y del disco y todos los datos 
+sudo parted -l # me da particiones 
+lsblk # 
