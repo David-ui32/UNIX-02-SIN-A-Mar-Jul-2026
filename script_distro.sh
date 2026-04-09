e813b88bac638cdf2bbd130b1e0408c1478226b5
David-ui32 <cristo123456xx@gmail.com>
2026-04-09 13:56:22 +0000

    make menuconfig

---
+++ script_distro.sh
@@ -0,0 +1,12 @@
+sudo apt update
+#Download the latest list of available packages from the repositories
+sudo apt upgrade
+#Install the latest versions of the packages you already have installed.
+sudo apt install
+#Install a specific package
+git clone --depth 1 https://github.com/torvalds/linux.git
+#copy a repository
+cd linux
+#What you do is change your current working directory to the folder called linux
+make menuconfig
+#It is a command used in Linux to configure the kernel before compiling it
