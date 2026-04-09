569fce684bceefc64da800c9b51e3e166e14af86
David-ui32 <cristo123456xx@gmail.com>
2026-04-09 12:24:26 +0000

    cd

---
+++ script_boot.sh
@@ -0,0 +1,4 @@
+mkdir ##Create a new folder with any name we assign to it
+ls  # list the folders I have
+cd.. #It takes me to the Dad or Mom folder
+cd # If I copy the path and paste it, it takes me directly to that folder.
