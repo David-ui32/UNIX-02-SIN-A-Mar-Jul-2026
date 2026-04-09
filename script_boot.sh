9590d179c35c4568d937262a0748becb71696a27
David-ui32 <cristo123456xx@gmail.com>
2026-04-09 12:38:34 +0000

    BASH

--- script_boot.sh
+++ script_boot.sh
@@ -2,3 +2,9 @@ mkdir ##Create a new folder with any name we assign to it
 ls  # list the folders I have
 cd.. #It takes me to the Dad or Mom folder
 cd # If I copy the path and paste it, it takes me directly to that folder.
+pwd #It takes me to the directory I'm in
+cd #I press enter it takes me directly to the home page
+/workspaces $ cd ~ #It also takes me to the home directory because of the tilde
+echo $HOME #shows me the route to get home
+echo $PATH #takes me to the path route
+echo $BASH #It takes me to the bash route 
