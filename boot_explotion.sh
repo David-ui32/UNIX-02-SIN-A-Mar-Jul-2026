fcc352f247584b64db1acac4b6c185f09928d607
David-ui32 <cristo123456xx@gmail.com>
2026-04-16 13:49:48 +0000

    ultimate changes..

--- boot_explotion.sh
+++ boot_explotion.sh
@@ -9,3 +9,12 @@ mkdir -- -rf #because it deletes without asking for confirmation and can delete 
 man ls #Open the manual for the ls command, which is used to list files and directories.    
 man git #Open the general Git manual, explaining what it is and how to use it   
 man git-clone #Open the specific git clone manual, showing all the options for cloning repositories
+chmod +x script.sh #Grants execution permission to everyone (user, group, and others)
+chmod u+x script.sh #Grants execution permission only to the file owner.
+chmod o-r secreto.txt #Remove read permission for “others”
+chmod u+r,go-rw privado #Grants read permission to the owner user and removes permissions
+
+echo "hola" sudo tee /etc/archivo_protegido > /dev/null #It allows writing to a protected file using sudo, but without displaying anything on the screen.
+ls /etc/ #What it does is list the contents of the directory
+
+cat /etc/archivo_protegido #Show the message that is in that folder
