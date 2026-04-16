0513831f1149016e6b7b030a97ee3609229309c8
David-ui32 <cristo123456xx@gmail.com>
2026-04-16 12:54:35 +0000

    mkdir -- rf

--- boot_explotion.sh
+++ boot_explotion.sh
@@ -1,2 +1,4 @@
-ls -l -a -h
-ls -lah
+ls -l -a -h #lists all files, including hidden ones, with detailed information and easy-to-read sizes
+ls -lah #Displays the contents of a directory with complete details
+mkdir --rm rf #because it deletes without asking for confirmation and can delete important data.
+mkdir -- -rf #because it deletes without asking for confirmation and can delete important data.
