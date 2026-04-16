d00f4ba14b24b6996922d150d08d57681b848639
David-ui32 <cristo123456xx@gmail.com>
2026-04-16 13:35:26 +0000

    ultimate changes

--- boot_explotion.sh
+++ boot_explotion.sh
@@ -2,3 +2,10 @@ ls -l -a -h #lists all files, including hidden ones, with detailed information a
 ls -lah #Displays the contents of a directory with complete details
 mkdir --rm rf #because it deletes without asking for confirmation and can delete important data.
 mkdir -- -rf #because it deletes without asking for confirmation and can delete important data.
+ --depth <depth>
+           Create a shallow clone with a history truncated to the specified number of commits. Implies --single-branch unless
+           --no-single-branch is given to fetch the histories near the tips of all branches. If you want to clone submodules
+           shallowly, also pass --shallow-submodules. #A value is missing, that's why it shows as shallow.
+man ls #Open the manual for the ls command, which is used to list files and directories.    
+man git #Open the general Git manual, explaining what it is and how to use it   
+man git-clone #Open the specific git clone manual, showing all the options for cloning repositories
