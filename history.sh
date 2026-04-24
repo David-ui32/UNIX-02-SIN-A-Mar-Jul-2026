3959098a65766bed94d72a5d7fb0a6cfd79dfe1c
David-ui32 <cristo123456xx@gmail.com>
2026-04-24 17:41:32 +0000

    primer problema resuelto

---
+++ history.sh
@@ -0,0 +1,12 @@
+6  mv kepler/docs kepler/registros
+    7  ls kepler 
+    8  pwd
+    9  ls
+   10  cd evaluation/
+   11  ls
+   12  ls kepler/
+   13  mv kepler/docs kepler/registros
+   14  mkdir kepler/plantillas
+   15  git add .
+   16  git commit -m "Fix problema 2: archivos movidos y renombrados"
+   17  history
