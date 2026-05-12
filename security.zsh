944d5d90739aec5d7db5eda33748fff638a20e6b
David-ui32 <cristo123456xx@gmail.com>
2026-05-12 17:06:07 +0000

    final

--- security.zsh
+++ security.zsh
@@ -1,4 +1,4 @@
-apt update
-apt install acl
-chow -R $(whoami) .
-setfacl -bnR .
+apt update #Update the list of available packages on your system
+apt install acl #Install the ACL (Access Control Lists) package, which allows for more advanced permission management.
+chow -R $(whoami) . #What it does is change the owner of all files and directories.
+setfacl -bnR . #The -b option deletes all ACLs (clears access control lists).
