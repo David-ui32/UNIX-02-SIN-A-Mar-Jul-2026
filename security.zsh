60cbf3384b6c3b34054d9d7282c03be5cba34e9c
David-ui32 <cristo123456xx@gmail.com>
2026-05-12 17:20:23 +0000

    chow luna

--- security.zsh
+++ security.zsh
@@ -7,3 +7,8 @@ touch secreto.txt #Create an empty file called secret.txt
 mkdir privado #Create a directory called "private" With umask 077
 ls -l # Lists the files and directories in the current directory in detail.
 umask 022 #returns to original values
+whoami #Displays the name of the current user who is running the session.
+echo "Hola" > mi_archivo #Create a file and write hello
+ls -l mi_archivo #Shows the details of the file my_file
+useradd -m -s /usr/bin/zsh luna #create user name luna 
+chown luna mi_archivo #Change the owner of the file my_file to the user luna.
