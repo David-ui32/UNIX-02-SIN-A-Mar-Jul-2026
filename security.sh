beee8370d2d4636c5074d2a097b9641756c9b9d4
David-ui32 <cristo123456xx@gmail.com>
2026-05-28 12:55:57 +0000

    newgrp.text

--- security.sh
+++ security.sh
@@ -3,3 +3,8 @@ id -gn #only main group name
 touch ~/test_grupo_heredado.txt
 ls -la ~/test_grupo_heredado.txt
 #the group it is the user's primary group
+(id -gn) #group main 
+touch ~/antes_de_nwegrp.txt  
+ls -la ~/antes_de_nwegrp.txt 
+#created a file before nwegrp
+id -gn #wiew group actually
