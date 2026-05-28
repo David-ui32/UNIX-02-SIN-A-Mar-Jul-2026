0affc0932b155203bf43e890cf5c095a66155e33
David-ui32 <cristo123456xx@gmail.com>
2026-05-28 13:34:34 +0000

    contraseña group

--- security.sh
+++ security.sh
@@ -8,3 +8,14 @@ touch ~/antes_de_nwegrp.txt
 ls -la ~/antes_de_nwegrp.txt 
 #created a file before nwegrp
 id -gn #wiew group actually
+newgrp desarrolladores # change the group to developers
+touch ~/dentro_de_newgrp.txt
+ls ~/dentro_de_newgrp.txt 
+# The group is desarrolador
+#create a directory 
+mkdir -p ~/proyecto_dev/src
+ls -la ~/
+exit #exit the group
+sudo groupadd grupo_restringido 
+ gpasswd grupo_restringido
+ #The system asks for a password for the group
