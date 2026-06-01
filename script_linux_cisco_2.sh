a3ce753248f0cf2295a0995827aa10369282ea84
David-ui32 <cristo123456xx@gmail.com>
2026-06-01 04:08:56 +0000

    commands of module 13

--- script_linux_cisco_2.sh
+++ script_linux_cisco_2.sh
@@ -1,13 +1,13 @@
 # Part 2 of the commands used in the Cisco course
 
-# module 11.1 
+# --- module 11.1 ---
 dd if=/dev/zero of=/tmp/swapex bs=1M count=50
 # Creates a 50 MB file filled with zeros.
 
 dd if=/dev/sda of=/dev/sdb
 # Copies the entire contents of one disk to another disk.
 
-# module 12
+# --- module 12 ---
 mv people.csv Work
 # Moves the file people.csv to the Work directory.
 
@@ -16,3 +16,13 @@ mv numbers.txt letters.txt alpha.txt School
 
 mv animals.txt zoo.txt
 # Renames animals.txt to zoo.txt.
+
+# --- module 13 ---
+rm linux.txt
+# Deletes the file linux.txt.
+
+rm Work
+# Attempts to remove the Work directory if it is empty.
+
+rm -r Work
+# Recursively removes the Work directory and its contents.
