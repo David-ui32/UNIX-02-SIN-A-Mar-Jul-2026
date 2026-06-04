a9e924a0901a778dbe0ca5d29c2ee98d449b0b71
David-ui32 <cristo123456xx@gmail.com>
2026-06-04 13:33:23 +0000

    result matematic

---
+++ blackhatbash1.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+bash --version
+env
+echo $[SHELL]
+echo $[random]
+echo $[UID]
+echo $[OSTYPE]
+man ls 
+ps-e -f
+df -h
+bash -r blackhatbash1.sh
+#bash -n blackhatbash1.sh
+#!/bin/bash
+set x 
+--snip--
+set +x 
