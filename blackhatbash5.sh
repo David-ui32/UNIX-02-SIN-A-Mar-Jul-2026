53677aa9636eda139ef392d532e46873facdb6a2
David-ui32 <cristo123456xx@gmail.com>
2026-06-05 17:15:34 +0000

    cat error

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -9,4 +9,7 @@ lzl || echo "el comnado lzl fallo"
  cat output.txt
  echo "Chao mundo!" >> output.txt
  cat output.txt
- 
+ $ ls -l / &> stdout_and_stderr.txt
+$ ls -l / 1> stdout.txt 2> stderr.txt
+lzl 2> error.txt
+cat error.txt
