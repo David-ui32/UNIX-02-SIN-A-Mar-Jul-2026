b825b0acd899aa4bc7e6a12cbc8a92d24415bfd2
David-ui32 <cristo123456xx@gmail.com>
2026-06-05 17:06:50 +0000

    hello mundo

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 set -x
 top&
+touch test && touch test123
+(ls; ps)
+ls; ps; whoami
+lzl || echo "el comnado lzl fallo"
+ echo "Hola mundo!" > output.txt
+ cat output.txt
+ echo "Chao mundo!" >> output.txt
+ cat output.txt
+ 
