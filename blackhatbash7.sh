931e889aba5d1322203f7f93a610c5752a65e5bc
David-ui32 <cristo123456xx@gmail.com>
2026-06-18 13:32:43 +0000

    class final

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -21,9 +21,10 @@ echo "Hola1,chao1">> example_csv.txt
 echo "Hola2,chao2">> example_csv.txt
 echo "Hola3,chao3">> example_csv.txt
 cat example_csv.txt
-
+####
 head log.txt
 awk 'NR < 10' log.txt
 grep "42.236.10.117" log.txt
 awk '{print $7}' log.txt 
 grep "42.236.10.117" log.txt | awk '{print $7}'
+
